9b21cce5e8594f8ddde2963cd1da4195153e8a12
Prajwal kumar <prajwalkumar@Prajwals-MacBook-Air.local>
2025-08-25 14:50:51 +0530

    corss resolved

--- backend/server.js
+++ backend/server.js
@@ -66,7 +66,6 @@ app.use((err, req, res, next) => {
 });
 
 const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, ()=>{
-    console.log(`Server is running on port ${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
